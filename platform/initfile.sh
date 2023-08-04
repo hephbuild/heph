09c9f5b9715c2faff658fccc1192f6dace5ea3f2
Raphael Vigee <raphvigee@gmail.com>
2023-08-04 12:14:57 +0100

    Add shell xrun

--- platform/initfile.sh
+++ platform/initfile.sh
@@ -1,10 +1,20 @@
-set -u
+set -uEo pipefail
 export PS1='$ '
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
 {{ if .Cmds }}
+trace-off() {
+  { set +x; } 2>/dev/null
+}
+
 run() {
 	{{.Cmds}}
+}
+xrun() {
+  trap trace-off ERR
+  set -x
+  {{.Cmds}}
+  trace-off
 }
 show() {
 	echo '{{.CmdsStr}}'
@@ -22,6 +32,7 @@ echo "=========== Help ==========="
 echo 'Shell mode, to exit; run exit or ctrl-d'
 {{ if .Cmds }}
 echo "run   : Runs the commands"
+echo "xrun  : Runs the commands with trace"
 echo "show  : Prints the commands"
 echo ''
 echo "============ Run ==========="
