c24f56deb8d38b88b5cf72df575e9945617ae39b
Raphael Vigee <raphvigee@gmail.com>
2023-09-26 16:54:45 +0100

    Build asm one by one

--- test/go/mod-asm/some_test.go
+++ test/go/mod-asm/some_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cloud.google.com/go/bigquery"
+	"github.com/apache/arrow/go/v12/arrow"
 	"testing"
 )
 
@@ -9,4 +10,5 @@ func TestSanity(t *testing.T) {
 	t.Log("Main - TestSanity Hello world")
 	// Simply require the package so that all deps get pulled too
 	_ = bigquery.Scope
+	_ = arrow.Second
 }
