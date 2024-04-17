afad5b092e5d70f95d7b142bdd2c70eefe9fdf8a
Raphael Vigee <raphvigee@gmail.com>
2024-04-17 17:16:36 +0100

    debug worker.group

--- utils/xdebug/fmt.go
+++ utils/xdebug/fmt.go
@@ -2,9 +2,12 @@ package xdebug
 
 import "fmt"
 
+const enabled = true
+
+// Sprintf is fmt.Sprintf, but zero-allocation/overhead when debug is disabled
 func Sprintf(format string, a ...any) string {
-	if false {
-		return format
+	if enabled {
+		return fmt.Sprintf(format, a...)
 	}
-	return fmt.Sprintf(format, a...)
+	return format
 }
