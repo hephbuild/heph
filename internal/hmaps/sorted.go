24a482f3035718de3fcc2f0d4003c794f2d4ec1c
Raphael Vigee <raphvigee@gmail.com>
2025-11-27 12:06:07 +0000

    prevent alloc

--- internal/hmaps/sorted.go
+++ internal/hmaps/sorted.go
@@ -9,6 +9,19 @@ import (
 
 func Sorted[Map ~map[K]V, K cmp.Ordered, V any](m Map) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
+		switch len(m) {
+		case 0:
+			return
+		case 1:
+			for k, v := range m {
+				if !yield(k, v) {
+					return
+				}
+			}
+
+			return
+		}
+
 		keys := slices.Sorted(maps.Keys(m))
 
 		for _, k := range keys {
