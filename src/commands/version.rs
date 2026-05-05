4144fffdbab54b5131192a939a5582f920a00171
Raphael Vigee <raphvigee@gmail.com>
2026-05-05 14:02:28 +0200

    lint

--- src/commands/version.rs
+++ src/commands/version.rs
@@ -1,21 +1,13 @@
-use std::convert::AsRef;
-use futures::TryStreamExt;
-use crate::commands::bootstrap;
-use crate::commands::utils::matcher_from_args;
-use crate::engine::get_cwp;
-
 #[derive(clap::Args)]
-#[command(override_usage = "run <TARGET_ADDRESS>\n       run <LABEL> <PACKAGE_MATCHER>")]
 pub struct Args {}
 
-const VERSION_OPT: Option<&str> = option_env!("HEPH_BUILD_VERSION");
+const VERSION: &str = match option_env!("HEPH_BUILD_VERSION") {
+    Some(val) => val,
+    None => "v0.0.0-dev",
+};
 
-fn version() -> &'static str {
-    VERSION_OPT.unwrap_or("v0.0.0-dev")
-}
-
-pub fn execute(args: &Args) -> anyhow::Result<()> {
-    println!("{}", version());
+pub fn execute(_args: &Args) -> anyhow::Result<()> {
+    println!("{}", VERSION);
 
     Ok(())
 }
